584dbd4fff716eddc2584d1420bad655be54efbc
vetri <vetrichelvi26@gmail.com>
2024-02-05 08:54:58 +0530

    studentcontroller

---
+++ app/Http/Controllers/Api/StudentsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentsController extends Controller
+{
+    //
+    public function index(){
+
+        $students = Student::all();
+
+        return response()->json($data,200);
+    }
+}
